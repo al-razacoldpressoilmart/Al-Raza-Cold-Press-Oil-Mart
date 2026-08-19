e7f795df5e6795b329dc928d4c6ecfd4da2a16dd
Al Raza Cold Press Oil <295314772+al-razacoldpressoilmart@users.noreply.github.com>
2026-08-20 02:24:22 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
 return {
-    base: '/Al-Raza-Cold-Press-Oil-Mart/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
